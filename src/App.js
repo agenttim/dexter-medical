7843fcb0fbba8a19362eb120e1d024693796a786
agent_tim <agent_tim@mail.ru>
2022-11-05 17:19:55 +0500

    2022.11.05

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const INITIAL_ANALYSIS = [
+    {
+        id: "1",
+        date: new Date(2018, 1, 21),
+        alanineAminotransferase: 45,
+        aspartateAminotransferase: 33,
+        alkalinePhosphatase: 72,
+        gammaGlutamyltransferase: 32,
+        totalProtein: 74,
+        totalBilirubin: 12.6,
+        bilirubinStraight: 2.3,
+        urea: 6.0,
+        creatinine: 100,
+        glucose: 5.2,
+        iron: 27.2,
+        cholesterol: 6.3
+    },
+    {
+        id: "2",
+        date: new Date(2022, 1, 14),
+        alanineAminotransferase: 36.7,
+        aspartateAminotransferase: 28,
+        alkalinePhosphatase: 0,
+        gammaGlutamyltransferase: 0,
+        totalProtein: 70.6,
+        totalBilirubin: 15.2,
+        bilirubinStraight: 0,
+        urea: 5.63,
+        creatinine: 93.3,
+        glucose: 5.27,
+        iron: 0,
+        cholesterol: 6.14
+    },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Начнем разработку приложения</h1>
+      </div>
   );
 }
 
